88a0cf7d2c4c8c51a344872a305b7b26726d20fd
DESKTOP-3KJM5FL\conta <contatobrandino@gmail.com>
2026-04-23 19:28:27 -0300

    Cria componente de simple-input

---
+++ src/app/components/simple-input/simple-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-input',
+  imports: [],
+  templateUrl: './simple-input.html',
+  styleUrl: './simple-input.scss',
+})
+export class SimpleInput {
+
+}
